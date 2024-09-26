d1231963fe9fba378916ceeafe8e0b1924e6e320
Leonardo Patrocinio <chicoripcurl1@gmail.com>
2024-09-26 16:46:06 -0500

    UpdateHome

--- src/app/public/pages/model/home/home.component.ts
+++ src/app/public/pages/model/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 export interface Tile {
   color: string;
@@ -7,22 +8,30 @@ export interface Tile {
   text: string;
 }
 
+export interface Refrigerator {
+  id: number;
+  title: string;
+  description: string;
+  capacity: string;
+  image: string;
+  temperature: string;
+  humidity: string;
+  lastMaintenance: string;
+  nextMaintenance: string;
+  model: string;
+  serialNumber: string;
+  installedDate: string;
+}
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
-
-  link = "https://cdn.discordapp.com/attachments/1273824394451615826/1288777187243200542/image_refrigerator.png?ex=66f66ac9&is=66f51949&hm=d6b30d99001e42b8e40f5fea5603c447ec1e667cb91d8fc6386a4ff55a126731&";
-  cards = [
-    { image: this.link, title: 'Camara A2', description: 'Activo', capacity: 'Capacidad: 80%' },
-    { image: this.link, title: 'Refrigerador EA2', description: 'En Mantenimiento', capacity: 'Capacidad: 0%'  },
-    { image: this.link, title: 'Refrigerador EA3', description: 'Activo', capacity: 'Capacidad: 50%' },
-    { image: this.link, title: 'Refrigerador EA6', description: 'Activo', capacity: 'Capacidad: 50%' },
-    // Añade más cards según sea necesario
-
-  ];
+export class HomeComponent implements OnInit {
+  refrigerators: Refrigerator[] = [];
+  selectedRefrigerator: Refrigerator | null = null;
+  mostrarDetalles = false;
 
   tiles: Tile[] = [
     {text: 'Notificaciones', cols: 3, rows: 1, color: 'lightblue'},
@@ -30,11 +39,35 @@ export class HomeComponent {
     {text: 'Colaboradores', cols: 1, rows: 1, color: '#8289EF'},
     {text: 'Sensores', cols: 2, rows: 1, color: '#262A73'},
   ];
-  constructor() {
-    console.log(this.cards); // Verifica que los datos se están cargando correctamente
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.loadRefrigerators();
+  }
+
+  loadRefrigerators() {
+    this.http.get<Refrigerator[]>('http://localhost:3000/refrigeration').subscribe(
+      data => {
+        this.refrigerators = data;
+      },
+      error => {
+        console.error('Error fetching refrigerators:', error);
+      }
+    );
   }
 
   getTextColor(description: string): string {
     return description.includes('Activo') ? 'green' : 'red';
+  }
+
+  viewDetails(refrigerator: Refrigerator) {
+    this.selectedRefrigerator = refrigerator;
+    this.mostrarDetalles = true;
+  }
+
+  cerrarDetalles() {
+    this.mostrarDetalles = false;
+    this.selectedRefrigerator = null;
   }
 }
