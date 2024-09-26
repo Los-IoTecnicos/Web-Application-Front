c11bb5fa83e4385bf452baa161b529354a988bd6
Prog-san <DieSanR@gmail.com>
2024-09-26 03:58:36 -0500

    fix: Image & Font of MatGridList Component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -21,12 +21,12 @@ export interface Tile {
 
 
 export class HomeComponent {
+  link = "https://cdn.discordapp.com/attachments/1273824394451615826/1288777187243200542/image_refrigerator.png?ex=66f66ac9&is=66f51949&hm=d6b30d99001e42b8e40f5fea5603c447ec1e667cb91d8fc6386a4ff55a126731&";
   cards = [
-    { image: 'src/assets/images/image_refrigerator.png', title: 'Camara A2', description: 'Activo' },
-    { image: "src/assets/images/image_refrigerator.png", title: 'Refrigerador EA2', description: 'En Mantenimiento' },
-    { image: "src/assets/images/image_refrigerator.png", title: 'Refrigerador EA3', description: 'Activo' },
-    { image: "src/assets/images/image_refrigerator.png", title: 'Refrigerador EA6', description: 'Activo' },
-    { image: "src/assets/images/image_refrigerator.png", title: 'Refrigerador EA8', description: 'Activo' },
+    { image: this.link, title: 'Camara A2', description: 'Activo', capacity: 'Capacidad: 80%' },
+    { image: this.link, title: 'Refrigerador EA2', description: 'En Mantenimiento', capacity: 'Capacidad: 0%'  },
+    { image: this.link, title: 'Refrigerador EA3', description: 'Activo', capacity: 'Capacidad: 50%' },
+    { image: this.link, title: 'Refrigerador EA6', description: 'Activo', capacity: 'Capacidad: 50%' },
     // Añade más cards según sea necesario
 
   ];
