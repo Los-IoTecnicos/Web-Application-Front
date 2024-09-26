865445385c28cb01da67bdea25bd87c42a1944d8
Prog-san <DieSanR@gmail.com>
2024-09-26 01:28:53 -0500

    feat: Add Angular Material & Component Grid List to Home Component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,22 +1,44 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from '../public/components/navbar/navbar.component';
+import { MatGridListModule} from "@angular/material/grid-list";
+
+export interface Tile {
+  color: string;
+  cols: number;
+  rows: number;
+  text: string;
+}
+
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, NavbarComponent],
+  imports: [CommonModule, NavbarComponent, MatGridListModule],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
+
+
+
 export class HomeComponent {
   cards = [
-    { image: '/assets/images/refrigerator.jpg', title: 'Refrigerador EA1', description: 'Estado: Activo' },
+    { image: '../../../assets/images/refrigerator.jpg', title: 'Refrigerador Prueba', description: 'Estado: Activo' },
     { image: "/assets/images/image_refrigerator.png", title: 'Refrigerador EA2', description: 'Estado: Activo' },
     { image: "/assets/images/image_refrigerator.png", title: 'Refrigerador EA3', description: 'Estado: Activo' },
     // Añade más cards según sea necesario
+
+  ];
+
+  tiles: Tile[] = [
+    {text: 'Notificaciones', cols: 3, rows: 1, color: 'lightblue'},
+    {text: 'Inventario', cols: 1, rows: 2, color: '#5D58D3'},
+    {text: 'Colaboradores', cols: 1, rows: 1, color: '#8289EF'},
+    {text: 'Sensores', cols: 2, rows: 1, color: '#262A73'},
   ];
   constructor() {
     console.log(this.cards); // Verifica que los datos se están cargando correctamente
   }
 
 }
+
+
